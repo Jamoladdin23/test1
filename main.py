a9a06e607345f553a2b7de5234e9eac155a6ee2d
Jamoladdin <djamazar23@gmail.com>
2023-09-13 19:15:59 +0200

    zanova

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 
 def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+    print('kil')
+    print('gkly')
 
 if __name__ == '__main__':
     print_hi('PyCharm')
